b71e1042d2821da0075490d3d20ce67080d9339c
Michele Rawlins <shellbop@att.net>
2020-08-01 12:22:15 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Mask_Sensitive_Information
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello Sir, What is your card number?");
+            var input = Console.ReadLine();
+
+            var length = input.Length;
+            var result = new String('X', length - 4) + input.Substring(length - 4);
+
+            Console.WriteLine(result);
+        }
+
+    }
+}
